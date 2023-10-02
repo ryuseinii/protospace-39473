a0500e097d0290c84af5bb9dcb795990dc092e84
ryuseinii <ryuseinii5519@gmail.com>
2023-10-02 21:45:51 +0900

    修正箇所①-7

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -21,7 +21,6 @@ class PrototypesController < ApplicationController
   end
 
   def show
-    @prototype = Prototype.find(params[:id])
     @comment = Comment.new
     @comments = @prototype.comments
   end
