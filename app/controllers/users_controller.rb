59389754c8c5ffc0f9b573a59be444cc01abb518
ryuseinii <ryuseinii5519@gmail.com>
2023-10-02 21:15:59 +0900

    修正箇所①-3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,5 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @name = user.name
-    @profile = user.profile
-    @occupation = user.occupation
-    @position = user.position
   end
 end
